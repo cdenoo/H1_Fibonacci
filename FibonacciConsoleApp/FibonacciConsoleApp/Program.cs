fdaf5466416cb90e54476b5d7c5dd5e2a022b4f8
cdenoo <9006411+cdenoo@users.noreply.github.com>
2018-09-24 11:40:54 +0200

    Added access modifiers Added a Fibonacci function

--- FibonacciConsoleApp/FibonacciConsoleApp/Program.cs
+++ FibonacciConsoleApp/FibonacciConsoleApp/Program.cs
@@ -6,10 +6,25 @@ using System.Threading.Tasks;
 
 namespace FibonacciConsoleApp
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            Console.WriteLine(Fibonacci(5));
+        }
+
+        // Returns the fibonacci number on a given position
+        private static int Fibonacci(int position)
+        {
+            int index = 0, previous = 0, next = 1, result = 0;
+            while(index < position)
+            {
+                result = previous;
+                previous = next;
+                next += result;
+                index++;
+            }
+            return result;
         }
     }
 }
